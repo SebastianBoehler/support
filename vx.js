835b8a96c1deadfe991f50da52bc6208e5432cbf
Sebastian_B <basti.boehler@hotmail.de>
2019-04-24 14:59:21 +0200

    s
    adsda

--- vx.js
+++ vx.js
@@ -67,11 +67,11 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
         message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
         const embed = new Discord.RichEmbed()
         .setColor(0xCF40FA)
-        .addField(`Hey @${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
+        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
         .setTimestamp();
         c.send({ embed: embed });
     }).catch(console.error);
-    await channel.setParent(`570592756515340289`)
+    channel.setParent(`570592756515340289`);
 
 }
 if (message.content.toLowerCase().startsWith(prefix + `close`)) {
